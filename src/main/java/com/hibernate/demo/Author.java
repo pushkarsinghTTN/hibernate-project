2e9762c0534c38d143f0ed8adcd808e4486f14cd
pushkarsinghTTN <pushkarsingh@tothenew.com>
2018-03-10 17:26:24 +0530

    hibernate

---
+++ src/main/java/com/hibernate/demo/Author.java
@@ -0,0 +1,48 @@
+package com.hibernate.demo;
+import javax.persistence.*;
+import java.util.*;
+
+@Entity
+public class Author{
+
+    @Id
+    private Integer id;
+    private String fname;
+    private String lname;
+    private Integer age;
+
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getFname() {
+        return fname;
+    }
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+    public String getLname() {
+        return lname;
+    }
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+    public Integer getAge() {
+        return age;
+    }
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", fname='" + fname + '\'' +
+                ", lname='" + lname + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
